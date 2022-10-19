a6e796584e11a3d66cef6ca1be4fd6c3ce99a9c7
qwdqwqwdqw <116013923+qwdqwqwdqw@users.noreply.github.com>
2022-10-19 19:55:32 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -36,10 +36,11 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 300
         self.alive = True
     def to_study(self):
         print("Time to Study")
-        self.progress += 0.12
+        self.progress += 0.75
         self.gladness -= 5
     def to_sleep(self):
         print("I will sleep")
@@ -47,24 +48,32 @@ class Student:
     def to_chill(self):
         print("Self Time")
         self.gladness+=5
-        self.progress-=0.1
+        self.progress-=0.5
+    def to_work(self):
+        print("No money?")
+        self.money += 40
+        self.gladness -= 7
+        self.progress += 0.5
     def is_alive(self):
         if self.progress < -0.5:
-            print("Cas Out")
             self.alive=False
         elif self.gladness <= 0:
             print("Depression")
             self.alive = False
-        elif self.progress > 0:
+        elif self.progress < 0:
             print("Passed externally")
             self.alive = False
+        elif self.money < 0:
+            print("Cash Out")
+            self.alive = False
+
     def end_of_day(self):
         print(f"Gladness = {self.gladness}")
         print(f"Progress = {self.progress}")
     def live(self, day):
         day = "Day" + str(day) + "of" + self.name + "life"
         print(f"{day:=^50}")
-        live_cube = random.randint(1,3)
+        live_cube = random.randint(1,4)
         if live_cube == 1:
             self.to_study()
         elif live_cube == 2:
@@ -73,10 +82,13 @@ class Student:
             self.to_chill()
             self.end_of_day()
             self.is_alive()
+        elif live_cube == 4:
+            self.to_work()
+            self.end_of_day()
+            self.is_alive()
 
 nick = Student(name = "Nick")
 for day in range(365):
     if nick.alive == False:
         break
     nick.live(day)
-
